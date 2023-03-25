3e89f8e443eb218d875b17b6ba46eeb92ad949cd
CodexX777 <shivang260279@gmail.com>
2023-03-26 04:27:16 +0530

    Configured App.js to provide sideBarContext globaly and also provided values for the sideBarcontext variables

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-import React from "react";
+import React,{useState,useCallback} from "react";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import Courses from "./pages/Courses";
 import CreatorAccess from "./pages/CreatorAccess";
@@ -11,28 +15,46 @@ import SendFeedback from "./pages/SendFeedback";
 import Trending from "./pages/Trending";
 import UserTour from "./pages/UserTour";
 import Sidebar from "./components/Sidebar";
+import MainNav from "./components/Navbar/MainNav";
+import { sideBarContext } from "./context/sideBarContext";
 
 const App = () => {
+
+  const [sideBarOpen, setSideBarOpen] = useState(false);
+
+  //to be moved to the navbar component
+  const sideBarToggle = useCallback(() => {
+    setSideBarOpen(!sideBarOpen);
+  },[sideBarOpen]);
+
   return (
-    <>
-      <Router>
-        <Sidebar>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/courses" element={<Courses />} />
-            <Route path="/creator" element={<CreatorAccess />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/discord" element={<Discord />} />
-            <Route path="/following" element={<Following />} />
-            <Route path="/sendfeedback" element={<SendFeedback />} />
-            <Route path="/trending" element={<Trending />} />
-            <Route path="/usertour" element={<UserTour />} />
-            <Route path="*" element={<>Not Found</>} />
-          </Routes>
-        </Sidebar>
-      </Router>
-    </>
+    <sideBarContext.Provider
+      value={{
+        sideBarToggle,
+        sideBarOpen,
+      }}
+    >
+      <BrowserRouter>
+        <div className="navigations">
+          <MainNav />
+          <Sidebar />
+        </div>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/courses" element={<Courses />} />
+          <Route path="/creator" element={<CreatorAccess />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/discord" element={<Discord />} />
+          <Route path="/following" element={<Following />} />
+          <Route path="/sendfeedback" element={<SendFeedback />} />
+          <Route path="/trending" element={<Trending />} />
+          <Route path="/usertour" element={<UserTour />} />
+          <Route path="*" element={<>Not Found</>} />
+        </Routes>
+      </BrowserRouter>
+    </sideBarContext.Provider>
   );
 };
 
