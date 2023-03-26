5e9e6542d1cdc7f0f2f6c0f65e9f00ee7d70847a
CodexX777 <shivang260279@gmail.com>
2023-03-26 08:52:44 +0530

    arranged layout of app.js to render MainNav, SideBar and Main-Content properly

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-import React,{useState,useCallback} from "react";
+import React, { useState, useCallback } from "react";
 import "./App.css";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Courses from "./pages/Courses";
 import CreatorAccess from "./pages/CreatorAccess";
@@ -17,15 +13,16 @@ import UserTour from "./pages/UserTour";
 import Sidebar from "./components/Sidebar";
 import MainNav from "./components/Navbar/MainNav";
 import { sideBarContext } from "./context/sideBarContext";
+import ProfilePage from "./pages/ProfilePage";
+
 
 const App = () => {
-
   const [sideBarOpen, setSideBarOpen] = useState(false);
 
   //to be moved to the navbar component
   const sideBarToggle = useCallback(() => {
     setSideBarOpen(!sideBarOpen);
-  },[sideBarOpen]);
+  }, [sideBarOpen]);
 
   return (
     <sideBarContext.Provider
@@ -34,25 +31,26 @@ const App = () => {
         sideBarOpen,
       }}
     >
+      <MainNav />
       <BrowserRouter>
-        <div className="navigations">
-          <MainNav />
+        <div className="main-page">
           <Sidebar />
+          <div className="content">
+            <Routes>
+              <Route path="/" element={<ProfilePage />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/courses" element={<Courses />} />
+              <Route path="/creator" element={<CreatorAccess />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/discord" element={<Discord />} />
+              <Route path="/following" element={<Following />} />
+              <Route path="/sendfeedback" element={<SendFeedback />} />
+              <Route path="/trending" element={<Trending />} />
+              <Route path="/usertour" element={<UserTour />} />
+              <Route path="*" element={<>Not Found</>} />
+            </Routes>
+          </div>
         </div>
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/courses" element={<Courses />} />
-          <Route path="/creator" element={<CreatorAccess />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/discord" element={<Discord />} />
-          <Route path="/following" element={<Following />} />
-          <Route path="/sendfeedback" element={<SendFeedback />} />
-          <Route path="/trending" element={<Trending />} />
-          <Route path="/usertour" element={<UserTour />} />
-          <Route path="*" element={<>Not Found</>} />
-        </Routes>
       </BrowserRouter>
     </sideBarContext.Provider>
   );
