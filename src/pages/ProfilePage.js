fb62e1a9b73a6c9ec007d5f7d903414adcce213a
CodexX777 <shivang260279@gmail.com>
2023-03-27 12:53:12 +0530

    created a new profilepage with new layout approach, added the aboutme component

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,17 +1,52 @@
 import React from "react";
 import "./ProfilePage.css";
+import Avatar from "../components/UIElements/Avatar";
+import { TbEditCircle } from "react-icons/tb";
+import AboutMe from "../components/User/AboutMe";
+
+let avatarUrl =
+  "https://lh3.googleusercontent.com/a/AGNmyxYRv_XBjPojMWq3Uv__44TEpK3JMtkqfPTxTo-oBw=s96-c";
+
+let UserName = "Shivang verma";
+let UserMail = "shivang260279@gmail.com";
+let followers = 0;
 
 const ProfilePage = () => {
   return (
     <>
-      <div className="main-content">
-        <div className="User-panel">User Data</div>
-        <div className="profile-panel">
-          <div className="profile-content">
-         
+      <div className="User-panel">
+        <div className="user-content">
+          <Avatar
+            className='user-avatar'
+            height="5"
+            width="5"
+            src={avatarUrl}
+            alt="account avatar"
+          />
+          <div className="pic-edit-cont">
+            <TbEditCircle className="pic-edit" />
+          </div>
+
+          <div className="greeting">
+            <h2>Hello,</h2>
+            <h1>{UserName}</h1>
+            <h4>{UserMail}</h4>
           </div>
         </div>
+        <div className="followers">
+          <h3>{followers} Followers</h3>
+        </div>
       </div>
+      <AboutMe />
+
+
+      {/* <div className="main-content">
+        <div className="User-panel">
+         
+        <div className="profile-panel">
+          <div className="profile-content"></div>
+        </div>
+      </div> */}
     </>
   );
 };
