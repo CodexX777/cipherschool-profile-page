fa659ac5e10a6d44861c6f96c73785eef1b5fe0c
CodexX777 <shivang260279@gmail.com>
2023-03-28 23:03:18 +0530

    added a heat map using react-calendar-heatmap

--- src/components/UIElements/Heatmap.js
+++ src/components/UIElements/Heatmap.js
@@ -1,52 +1,38 @@
 import React from "react";
+import CalendarHeatmap from "react-calendar-heatmap";
+import "react-calendar-heatmap/dist/styles.css";
 import "./Heatmap.css";
 
-
-
+const data = [
+  { date: "2022-01-01", count: 2 },
+  { date: "2022-01-02", count: 3 },
+  { date: "2022-01-03", count: 1 },
+  // add more data here
+];
 
 const Heatmap = () => {
-  const data = [
-    { date: "2022-01-01", value: 5 },
-    { date: "2022-01-02", value: 10 },
-    { date: "2022-01-03", value: 7 },
-    // add more data here
-  ];
-
-  const startDate = new Date("2022-01-01");
-  const endDate = new Date("2022-12-31");
-  const containerWidth = 800;
-  const cellSize = containerWidth / 365;
-
-  const calculateCellPositionAndSize = (date) => {
-    const timeDiff = date.getTime() - startDate.getTime();
-    const dayDiff = timeDiff / (1000 * 3600 * 24);
-    const x = dayDiff * cellSize;
-    const y = 0;
-    const width = cellSize;
-    const height = cellSize;
-    return { x, y, width, height };
-  };
-
   return (
-    <div className="heatmap">
-      {data.map((dataPoint) => {
-        const { date, value } = dataPoint;
-        const cellPositionAndSize = calculateCellPositionAndSize(
-          new Date(date)
-        );
-        return (
-          <div
-            className="heatmap-cell"
-            key={date}
-            style={{
-              transform: `translate(${cellPositionAndSize.x}rem, ${cellPositionAndSize.y}rem)`,
-              width: `${cellPositionAndSize.width}rem`,
-              height: `${cellPositionAndSize.height}rem`,
-              backgroundColor: `rgba(255, 0, 0, ${value / 10})`,
-            }}
-          ></div>
-        );
-      })}
+    <div className="heatmap-cont">
+      <h3 className="heatmap-heading">CIPHER MAP</h3>
+      <CalendarHeatmap
+        startDate={new Date("2022-01-01")}
+        endDate={new Date("2022-12-31")}
+        values={data}
+        showWeekdayLabels={true}
+        titleForValue={(value) => {
+          if (value && value.date) {
+            // return the date as the title for each cell if value and value.date are defined
+            return value.date;
+          }
+          return null;
+        }}
+        classForValue={(value) => {
+          if (!value) {
+            return "color-empty";
+          }
+          return `color-scale-${value.count}`;
+        }}
+      />
     </div>
   );
 };
